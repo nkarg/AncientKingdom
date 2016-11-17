ef1240a5c75ca04c9a32c9b6a7b3a6ae3982e323
Ricardo <ricardogauto11@gmail.com>
2016-11-17 15:00:12 -0300

    Login terminado

--- AncientKingdom/Controllers/AccountController.cs
+++ AncientKingdom/Controllers/AccountController.cs
@@ -75,7 +75,7 @@ namespace AncientKingdom.Controllers
 
             // This doesn't count login failures towards account lockout
             // To enable password failures to trigger account lockout, change to shouldLockout: true
-            var result = await SignInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, shouldLockout: false);
+            var result = await SignInManager.PasswordSignInAsync(model.Nickname, model.Password, model.RememberMe, shouldLockout: false);
             switch (result)
             {
                 case SignInStatus.Success:
@@ -151,7 +151,7 @@ namespace AncientKingdom.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new ApplicationUser { UserName = model.Nickname, Email = model.Email };
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
@@ -202,7 +202,8 @@ namespace AncientKingdom.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = await UserManager.FindByNameAsync(model.Email);
+                //var user = await UserManager.FindByNameAsync(model.Email);
+                var user = await UserManager.FindByEmailAsync(model.Email);
                 if (user == null || !(await UserManager.IsEmailConfirmedAsync(user.Id)))
                 {
                     // Don't reveal that the user does not exist or is not confirmed
--- AncientKingdom/Models/AccountViewModels.cs
+++ AncientKingdom/Models/AccountViewModels.cs
@@ -49,9 +49,8 @@ namespace AncientKingdom.Models
     public class LoginViewModel
     {
         [Required]
-        [Display(Name = "Email")]
-        [EmailAddress]
-        public string Email { get; set; }
+        [Display(Name = "Nickname")]
+        public string Nickname { get; set; }
 
         [Required]
         [DataType(DataType.Password)]
@@ -64,6 +63,10 @@ namespace AncientKingdom.Models
 
     public class RegisterViewModel
     {
+        [Required]
+        [Display(Name = "Nickname")]
+        public string Nickname { get; set; }
+
         [Required]
         [EmailAddress]
         [Display(Name = "Email")]

5eb9b416f5421d752969d83eac3de4f88426edc4
nkarg <r.cadenas.edu@gmail.com>
2016-11-29 17:26:12 -0300

    Moves and Timer

--- AncientKingdom/Scripts/indexWizard.js
+++ AncientKingdom/Scripts/indexWizard.js
@@ -1,12 +1,35 @@
 ﻿$(document).ready(function () {
+
+    var $success = $('.success'),
+    $successMsg = $('.success-message'),
+    $btnContinue = $('.btn-continue'),
+    visibleClass = 'is-visible',
+    dataType = 'data-type',
+    turnsCount = 2,
+    attemptsCount = 0,
+    timeoutLength = 600,
+    gameID = 3,
+    msg;
+
     counter = 0;
     var $btnSound = $('.btn-sound'),
     $themeSongEl = $('#theme-song')[0],
     playSoundClass = 'is-playing';
 
+    var timer = setInterval(function() {
+
+        var count = parseInt($('#theTarget').html());
+        if (count !== 0) {
+            $('#theTarget').html(count - 1);
+        } else {
+            clearInterval(timer);
+        }
+    }, 1000);
+
+
     $(".board label").on('click', function () {
         counter = counter + 1;
-
+        $("#movesCount").text(counter);
         var isChecked = true
         $(".correctRadio").each(function () {
             if (!$(this).prop("checked")) {
@@ -16,7 +39,10 @@
         if (isChecked) {
             //esta completo 
         }
-
+        if (counter == 5) {
+            $success.addClass(visibleClass);
+            
+        }
     });
     $btnSound.click(function (e) {
         e.preventDefault();
